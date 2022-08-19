e5f90d0803f1d484216ef42ec52fc8cd014df23f
aneesh072 <aneeshkarki292@gmail.com>
2022-08-19 22:43:50 +1000

    setting up icons as per the condition of weather

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import './App.css';
 import {
   IoMdSunny,
   IoMdRainy,
@@ -24,7 +25,7 @@ const APIkey = '200905de7c747b8357845382b172ca68';
 
 const App = () => {
   const [data, setData] = useState(null);
-  const [location, setLocation] = useState('Sydney');
+  const [location, setLocation] = useState('Danmark');
 
   useEffect(() => {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${APIkey}`;
@@ -33,9 +34,47 @@ const App = () => {
       setData(res.data);
     });
   }, [location]);
-  console.log(data);
 
-  return <div className="flex justify-center ..."></div>;
+  //Loader if the data is not fetching or false
+  if (!data) {
+    return (
+      <div>
+        {' '}
+        <ImSpinner8 className="text-5xl animate-spin" />
+      </div>
+    );
+  }
+
+  //icons setting accroding to the ewathers
+
+  let icon;
+  console.log(data.weather[0].main);
+
+  switch (data.weather[0].main) {
+    case 'Clouds':
+      icon = <IoMdCloudy />;
+      break;
+    case 'Haze':
+      icon = <BsCloudHaze2Fill />;
+      break;
+    case 'Rain':
+      icon = <IoMdRainy />;
+      break;
+    case 'Clear':
+      icon = <IoMdSunny />;
+      break;
+    case 'Drizzle':
+      icon = <BsCloudDrizzleFill />;
+      break;
+    case 'Snow':
+      icon = <IoMdSnow />;
+      break;
+    case 'Thunderstorm':
+      icon = <IoMdThunderstorm />;
+      break;
+  }
+
+  return <div className="flex justify-center ...">{icon}</div>;
 };
 
 export default App;
